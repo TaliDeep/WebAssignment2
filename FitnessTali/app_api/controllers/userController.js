e29fb640e7fc3795c54abb07fccdf891c3bd0383
Mehmetali Duman <mehmetali_1222@hotmail.com>
2019-11-26 14:39:25 +0100

    updated error

--- FitnessTali/app_api/controllers/userController.js
+++ FitnessTali/app_api/controllers/userController.js
@@ -4,12 +4,9 @@ const User = require('../Models/userModel.js');
 
 
 
-  module.exports.register = async function (req, res) {
-    console.log(user);
-    const user = new User(req.body);
 
-    // user.save()
-    //   .then((user) => user.generateAuthToken())
-    //   .then((token) => res.status(201).json({token: token}))
-    //   .catch((err) => res.status(409).send(err))
+  module.exports.register = async function (req, res) {
+    console.log(req.body);
+    User.create(req.body);
+
   }
